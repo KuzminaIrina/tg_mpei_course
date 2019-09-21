17926708c845fb17765902ce1953e97094a10bd2
IK <irina.kuzmina1207@mail.ru>
2019-09-21 13:39:09 +0300

    50_Pow

---
+++ 50_Pow.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)
+            return 1.0;
+        if(n < 0)
+            return 1/(x * myPow(x, -(n+ 1)));
+        double p = myPow(x, n/2);
+        return n & 1 ? p * p * x : p * p;
+    }
+};
