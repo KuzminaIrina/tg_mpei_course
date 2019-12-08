8aede9fc3c7493cda50008110428f85591433cfe
KuzminaIrina <54815110+KuzminaIrina@users.noreply.github.com>
2019-12-08 18:20:56 +0300

    Update 417.cpp

--- 417.cpp
+++ 417.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/pacific-atlantic-water-flow/description/
+https://leetcode.com/problems/pacific-atlantic-water-flow/
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
